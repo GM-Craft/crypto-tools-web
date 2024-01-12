cbe6220f225c57a2fa10e3a759c5c4911d521a1a
arthur <chenzhiguook@foxmail.com>
2024-01-12 23:25:27 +0800

    删除测试代码

--- backends/handlers/commons.py
+++ backends/handlers/commons.py
@@ -11,12 +11,5 @@ index = CAppRouter('')
 @index('/')
 class IndexPage(RequestHandler):
     def get(self):
-        logger.info('index page')
+
         self.render('index.html')
-
-
-@index('/next')
-class IndexPage2(RequestHandler):
-    def get(self):
-        logger.info('index page')
-        self.write('hello')
